e94ec4779b8181aab9b5db0db8abd49d4cef15e7
jufehuva <juanf.hurtadov@gmail.com>
2021-11-22 14:09:01 -0500

    13

--- js/src.js
+++ js/src.js
@@ -1,5 +1,7 @@
 let operaciones=[];
 let balance=0;
+let metaAhorro=0;
+
 
 function registrarOperacion(monto, tipo, categoria, fecha) {
     operaciones.push(
@@ -19,18 +21,30 @@ function registrarOperacion(monto, tipo, categoria, fecha) {
     localStorage.setItem("operaciones",JSON.stringify(operaciones));
     localStorage.setItem("balance",balance);
 }
+
 function registrarMetaAhorro(meta) {
     localStorage.setItem("metaAhorro", meta);
 }
+
 function estaMiMetaCumplida() {
-    if (Number(localStorage.getItem("metaAhorro")) < Number(localStorage.getItem("balance"))) {
+    metaAhorro=parseFloat(localStorage.getItem("metaAhorro"));
+    balance=parseFloat(localStorage.getItem("balance"));
+    if (metaAhorro<balance) {
+        console.log("cumple");
         return 'cumple';
     } else {
+        console.log("no cumple");
         return "no cumple";
     }
 }
-
+registrarMetaAhorro(5000);
+registrarOperacion(2000,'ingreso' , 'salario', '10/01/2015');
+registrarOperacion(5000,'ingreso' , 'salario', '10/01/2015');
+registrarOperacion(1200,'gasto' , 'salario', '10/01/2015');
+estaMiMetaCumplida();
 module.exports.registrarMetaAhorro=registrarMetaAhorro;
 module.exports.estaMiMetaCumplida=estaMiMetaCumplida;
 module.exports.registrarOperacion=registrarOperacion;
+
+
 
