1de74aa452cdc5a1bb466ec2c6b446497f82f442
jufehuva <juanf.hurtadov@gmail.com>
2021-11-21 22:15:48 -0500

    12

--- js/src.js
+++ js/src.js
@@ -1,73 +1,36 @@
 let operaciones=[];
 let balance=0;
 
-const registrarOperacion=(monto,tipo,categoria,fecha)=>{
+function registrarOperacion(monto, tipo, categoria, fecha) {
     operaciones.push(
         {
-            monto: x,
-            tipo: gasto-ingreso,
-            categoria: x,
-            fecha: date
+            monto: monto,
+            tipo: tipo,
+            categoria: categoria,
+            fecha: fecha
         }
     );
-}
-const registrarMetaAhorro=(meta)=>{}
-const estaMiMetaCumplida=()=>{}
-
-
-//FUNCION
-//var operaciones = [];
-//var balance = 0;
-
-function registrarOperacion(monto, tipo, categoria, fecha) {
-    //OBJETO
-    let objeto1 = {
-        ingresodemonto: monto,
-        ingresodetipo: tipo,
-        ingresodecategoria: categoria,
-        ingresodefecha: fecha
+    if (tipo == 'ingreso') {
+        balance = balance + monto;
     }
-    //METER EL OBJETO EN EL ARRAY
-    operaciones.push(objeto1)
-
-    //CONDICIONAL SI ES IGUAL A INGRESO SUME DE LO CONTRARIO RESTE
-    if (tipo == 'Ingreso') {
-        balance = balance+monto;
-    }else{
-        balance
-let operaciones = [];
-let balance = 0;
-function parametros(params) {
-    
+    else if (tipo == 'gasto') {
+        balance = balance - monto;
+    }
+    localStorage.setItem("operaciones",JSON.stringify(operaciones));
+    localStorage.setItem("balance",balance);
+}
+function registrarMetaAhorro(meta) {
+    localStorage.setItem("metaAhorro", meta);
+}
+function estaMiMetaCumplida() {
+    if (Number(localStorage.getItem("metaAhorro")) < Number(localStorage.getItem("balance"))) {
+        return 'cumple';
+    } else {
+        return "no cumple";
+    }
 }
 
-const registrarOperacion = (monto, tipo, categoria, fecha)=>{
-    console.log(operaciones); 
-    valMonto = parseInt(monto);
-
-    console.log(valMonto);
-    console.log(tipo);
-    console.log(categoria);
-    console.log(fecha);
-
-    operaciones.push({
-        montoVal:console.log(operaciones),
-        tipoVal: tipo,
-        categoriaVal: categoria,
-        fechaVal: fecha
-    });
-}
-
-console.log(operaciones);
-
-
-const registrarMetaAhorro=(meta)=>{
-
-}
-
-const estaMiMetaCumplida=()=>{
-
-}
 module.exports.registrarMetaAhorro=registrarMetaAhorro;
 module.exports.estaMiMetaCumplida=estaMiMetaCumplida;
 module.exports.registrarOperacion=registrarOperacion;
+
